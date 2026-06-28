ea205e766e6b38b0a0b912d7b7b5b40403df29b4
Alexander Postol <grief.north@gmail.com>
2026-06-28 20:17:02 +0300

    Add torrserver package (MatriX.141.9) with NixOS and home-manager modules

--- default.nix
+++ default.nix
@@ -3,4 +3,9 @@
 }:
 {
   throne = pkgs.callPackage ./pkgs/throne { };
+
+  torrserver = pkgs.callPackage ./pkgs/torrserver { };
+
+  nixosModules.torrserver = ./modules/torrserver/nixos.nix;
+  homeModules.torrserver = ./modules/torrserver/home-manager.nix;
 }
